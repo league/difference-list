3c5600f7c91d43c152234b01a97b98e8db1a7e59
Christopher League <league@contrapunctus.net>
2016-05-23 23:27:14 -0400

    Initial elm-package.json and minimal module.

---
+++ src/DList.elm
@@ -0,0 +1,14 @@
+module DList exposing
+  ( DList
+  )
+
+{-| A representation of lists with an efficient append operation.
+
+# Type
+@docs DList
+
+-}
+
+
+{-| Blah the type. -}
+type alias DList = Int
