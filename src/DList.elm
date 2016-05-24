ecd7b9f280bb0dfb62deef8140786892b2b62b53
Christopher League <league@contrapunctus.net>
2016-05-23 23:50:09 -0400

    Start on some essential functions, missing docs.

--- src/DList.elm
+++ src/DList.elm
@@ -1,18 +1,34 @@
 module DList exposing
   ( DList
-  , floop
+  , toList
+  , empty
+  , singleton
+  , fromList
   )
 
 {-| A representation of lists with an efficient append operation.
 
 # Type
-@docs DList
+@docs DList, toList, empty, singleton, fromList
 
 -}
 
 
 {-| Blah the type. -}
-type alias DList = Int
+type DList a = DList (List a -> List a)
 
-floop : DList
-floop = 32
+{-| whatever. -}
+empty : DList a
+empty = DList identity
+
+{-| TODO -}
+toList : DList a -> List a
+toList (DList f) = f []
+
+{-| TODO -}
+singleton : a -> DList a
+singleton = DList << (::)
+
+{-| TODO -}
+fromList : List a -> DList a
+fromList = DList << (++)
--- tests/Test.elm
+++ tests/Test.elm
@@ -5,12 +5,26 @@ import Html exposing (pre, text)
 import DList exposing (..)
 
 tests : Test
-tests =
-  suite "Test the test framework"
-          [ test "minimal" <| assertEqual 3 (1+2)
-          , test "dlist" <| assertEqual 32 floop
-          ]
+tests = suite "Difference lists"
+        [ test "empty list is empty"
+          <| assertEqual []
+          <| toList empty
+
+        , test "singleton list contains one thing"
+          <| assertEqual [19]
+          <| toList
+          <| singleton 19
+
+        , test "from-to empty list"
+          <| assertEqual []
+          <| toList
+          <| fromList []
+
+        , test "from-to non-empty list"
+          <| assertEqual [18,21,74,8]
+          <| toList
+          <| fromList [18,21,74,8]
+        ]
 
 main =
-  pre [] [text (stringRunner tests)]
---  runSuite tests
+  runSuite tests
