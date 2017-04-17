4e115d31654f657f00e1d39702baa617a44171a5
Christopher League <league@contrapunctus.net>
2017-04-17 14:18:38 -0400

    Begin update to 0.18.0

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,103 +1,105 @@
 module Main exposing (..)
 
-import ElmTest exposing (..)
 import DList exposing (..)
+import Expect exposing (equal)
+import Test exposing (Test, describe, test)
+import Test.Runner.Html
 
 tests : Test
-tests = suite "Difference lists"
+tests = describe "Difference lists"
         [ test "empty list is empty"
-          <| assertEqual []
+          <| \() -> equal []
           <| toList empty
 
         , test "singleton list contains one thing"
-          <| assertEqual [19]
+          <| \() -> equal [19]
           <| toList
           <| singleton 19
 
         , test "from-to empty list"
-          <| assertEqual []
+          <| \() -> equal []
           <| toList
           <| fromList []
 
         , test "from-to non-empty list"
-          <| assertEqual [18,21,74,8]
+          <| \() -> equal [18,21,74,8]
           <| toList
           <| fromList [18,21,74,8]
 
         , test "cons onto empty list"
-          <| assertEqual [23]
+          <| \() -> equal [23]
           <| toList
           <| cons 23 empty
 
         , test "cons onto non-empty list"
-          <| assertEqual [40, 21, 18]
+          <| \() -> equal [40, 21, 18]
           <| toList
           <| cons 40
           <| fromList [21, 18]
 
         , test "snoc onto empty list"
-          <| assertEqual [23]
+          <| \() -> equal [23]
           <| toList
           <| snoc empty 23
 
         , test "snoc onto non-empty list"
-          <| assertEqual [21, 18, 40]
+          <| \() -> equal [21, 18, 40]
           <| toList
           <| snoc (fromList [21, 18]) 40
 
         , test "append two non-empty lists"
-          <| assertEqual [34, 18, 91, 70]
+          <| \() -> equal [34, 18, 91, 70]
           <| toList
           <| append (fromList [34, 18]) (fromList [91, 70])
 
         , test "append three singleton lists, right-associative"
-          <| assertEqual [18, 19, 20]
+          <| \() -> equal [18, 19, 20]
           <| toList
           <| append (singleton 18)
           <| append (singleton 19) (singleton 20)
 
         , test "append three singleton lists, left-associative"
-          <| assertEqual [18, 19, 20]
+          <| \() -> equal [18, 19, 20]
           <| toList
           <| append (append (singleton 18) (singleton 19)) (singleton 20)
 
         , test "append with empty list on left"
-          <| assertEqual [40, 21]
+          <| \() -> equal [40, 21]
           <| toList
           <| append empty (fromList [40, 21])
 
         , test "append with empty list on right"
-          <| assertEqual [40, 21]
+          <| \() -> equal [40, 21]
           <| toList
           <| append (fromList [40, 21]) empty
 
         , test "append two empty lists"
-          <| assertEqual []
+          <| \() -> equal []
           <| toList
           <| append empty empty
 
         , test "append using operator"
-          <| assertEqual [13, 81, 19]
+          <| \() -> equal [13, 81, 19]
           <| toList
           <| singleton 13 +&+ fromList [81] +&+ empty +&+ singleton 19 +&+ empty
 
         , test "map over a non-empty list"
-          <| assertEqual ["21", "88", "93"]
+          <| \() -> equal ["21", "88", "93"]
           <| toList
           <| map toString
           <| fromList [21, 88, 93]
 
         , test "fold over an empty list"
-          <| assertEqual 1
+          <| \() -> equal 1
           <| foldr (*) 1 empty
 
         , test "fold over non-empty list"
-          <| assertEqual 120
+          <| \() -> equal 120
           <| foldr (*) 1
           <| fromList [5,4,3,2]
 
         , test "concat a list of dlists"
-          <| assertEqual [13, 81, 99, 40]
+          <| \() -> equal [13, 81, 99, 40]
           <| toList
           <| concat [ singleton 13
                     , empty
@@ -108,7 +110,7 @@ tests = suite "Difference lists"
                     ]
 
         , test "intersperse a list into a list of dlists"
-          <| assertEqual [15, 0, 81, 17, 0, 99]
+          <| \() -> equal [15, 0, 81, 17, 0, 99]
           <| toList
           <| intersperse (singleton 0)
              [ singleton 15
@@ -118,4 +120,4 @@ tests = suite "Difference lists"
         ]
 
 main =
-  runSuite tests
+    Test.Runner.Html.run tests
