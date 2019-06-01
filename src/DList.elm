b03594559dd14533b6c14c69d97fb1c8e9e5a9af
Christopher League <league@contrapunctus.net>
2019-05-31 23:45:40 -0400

    Run elm-format on source files

--- src/DList.elm
+++ src/DList.elm
@@ -1,17 +1,8 @@
 module DList exposing
-  ( DList
-  , append
-  , concat
-  , cons
-  , empty
-  , foldr
-  , fromList
-  , intersperse
-  , map
-  , singleton
-  , snoc
-  , toList
-  )
+    ( DList, toList
+    , empty, singleton, cons, snoc, append, fromList
+    , concat, foldr, intersperse, map
+    )
 
 {-| A representation of lists with an efficient append operation.
 
@@ -31,59 +22,98 @@ Some limitations of the `DList` representation are:
 
 @docs DList, toList
 
+
 # Construction
+
 @docs empty, singleton, cons, snoc, append, fromList
 
+
 # Processing
+
 @docs concat, foldr, intersperse, map
 
 -}
 
 
-{-| A difference list containing elements of type `a`. -}
-type DList a = DList (List a -> List a)
+{-| A difference list containing elements of type `a`.
+-}
+type DList a
+    = DList (List a -> List a)
+
 
 {-| Convert to a regular Elm `List` by applying the underlying function
-    representation. -}
+representation.
+-}
 toList : DList a -> List a
-toList (DList f) = f []
+toList (DList f) =
+    f []
 
-{-| The empty list. -}
+
+{-| The empty list.
+-}
 empty : DList a
-empty = DList identity
+empty =
+    DList identity
 
-{-| Construct a list containing one element. -}
+
+{-| Construct a list containing one element.
+-}
 singleton : a -> DList a
-singleton = DList << (::)
+singleton =
+    DList << (::)
 
-{-| Convert a regular Elm `List` to a difference list. -}
+
+{-| Convert a regular Elm `List` to a difference list.
+-}
 fromList : List a -> DList a
-fromList = DList << (++)
+fromList =
+    DList << (++)
 
-{-| Prepend an element onto the front of the list. -}
+
+{-| Prepend an element onto the front of the list.
+-}
 cons : a -> DList a -> DList a
-cons x (DList f) = DList <| (::) x << f
+cons x (DList f) =
+    DList <| (::) x << f
 
-{-| Add an element onto the back of the list. O(1) just like `append`. -}
+
+{-| Add an element onto the back of the list. O(1) just like `append`.
+-}
 snoc : DList a -> a -> DList a
-snoc (DList f) x = DList <| f << (::) x
+snoc (DList f) x =
+    DList <| f << (::) x
 
-{-| Concatenate two lists. -}
+
+{-| Concatenate two lists.
+-}
 append : DList a -> DList a -> DList a
-append (DList f) (DList g) = DList <| f << g
+append (DList f) (DList g) =
+    DList <| f << g
 
-{-| Iterate through a `DList`. -}
+
+{-| Iterate through a `DList`.
+-}
 foldr : (a -> b -> b) -> b -> DList a -> b
-foldr f b = List.foldr f b << toList
+foldr f b =
+    List.foldr f b << toList
 
-{-| Apply a function to each element, accumulating the results as a `DList`. -}
+
+{-| Apply a function to each element, accumulating the results as a `DList`.
+-}
 map : (a -> b) -> DList a -> DList b
-map f = foldr (cons << f) empty
+map f =
+    foldr (cons << f) empty
 
-{-| Concatenate a list of difference lists. -}
-concat : List(DList a) -> DList a
-concat = List.foldr append empty
 
-{-| Intersperse the contents of a `DList` between other lists. -}
-intersperse : DList a -> List(DList a) -> DList a
-intersperse x xs = concat <| List.intersperse x xs
+{-| Concatenate a list of difference lists.
+-}
+concat : List (DList a) -> DList a
+concat =
+    List.foldr append empty
+
+
+{-| Intersperse the contents of a `DList` between other lists.
+-}
+intersperse : DList a -> List (DList a) -> DList a
+intersperse x xs =
+    concat <| List.intersperse x xs
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,115 +1,118 @@
-module Tests exposing (..)
+module Tests exposing (suite)
 
+import DList exposing (..)
 import Expect exposing (equal)
 import Test exposing (Test, describe, test)
-import DList exposing (..)
+
 
 suite : Test
 suite =
-  describe "Difference lists"
-        [ test "empty list is empty"
-          <| \() -> equal []
-          <| toList empty
-
-        , test "singleton list contains one thing"
-          <| \() -> equal [19]
-          <| toList
-          <| singleton 19
-
-        , test "from-to empty list"
-          <| \() -> equal []
-          <| toList
-          <| fromList []
-
-        , test "from-to non-empty list"
-          <| \() -> equal [18,21,74,8]
-          <| toList
-          <| fromList [18,21,74,8]
-
-        , test "cons onto empty list"
-          <| \() -> equal [23]
-          <| toList
-          <| cons 23 empty
-
-        , test "cons onto non-empty list"
-          <| \() -> equal [40, 21, 18]
-          <| toList
-          <| cons 40
-          <| fromList [21, 18]
-
-        , test "snoc onto empty list"
-          <| \() -> equal [23]
-          <| toList
-          <| snoc empty 23
-
-        , test "snoc onto non-empty list"
-          <| \() -> equal [21, 18, 40]
-          <| toList
-          <| snoc (fromList [21, 18]) 40
-
-        , test "append two non-empty lists"
-          <| \() -> equal [34, 18, 91, 70]
-          <| toList
-          <| append (fromList [34, 18]) (fromList [91, 70])
-
-        , test "append three singleton lists, right-associative"
-          <| \() -> equal [18, 19, 20]
-          <| toList
-          <| append (singleton 18)
-          <| append (singleton 19) (singleton 20)
-
-        , test "append three singleton lists, left-associative"
-          <| \() -> equal [18, 19, 20]
-          <| toList
-          <| append (append (singleton 18) (singleton 19)) (singleton 20)
-
-        , test "append with empty list on left"
-          <| \() -> equal [40, 21]
-          <| toList
-          <| append empty (fromList [40, 21])
-
-        , test "append with empty list on right"
-          <| \() -> equal [40, 21]
-          <| toList
-          <| append (fromList [40, 21]) empty
-
-        , test "append two empty lists"
-          <| \() -> equal []
-          <| toList
-          <| append empty empty
-
-        , test "map over a non-empty list"
-          <| \() -> equal ["21", "88", "93"]
-          <| toList
-          <| map String.fromInt
-          <| fromList [21, 88, 93]
-
-        , test "fold over an empty list"
-          <| \() -> equal 1
-          <| foldr (*) 1 empty
-
-        , test "fold over non-empty list"
-          <| \() -> equal 120
-          <| foldr (*) 1
-          <| fromList [5,4,3,2]
-
-        , test "concat a list of dlists"
-          <| \() -> equal [13, 81, 99, 40]
-          <| toList
-          <| concat [ singleton 13
-                    , empty
-                    , fromList [81, 99]
-                    , empty
-                    , singleton 40
-                    , empty
-                    ]
-
-        , test "intersperse a list into a list of dlists"
-          <| \() -> equal [15, 0, 81, 17, 0, 99]
-          <| toList
-          <| intersperse (singleton 0)
-             [ singleton 15
-             , fromList [81, 17]
-             , singleton 99
-             ]
+    describe "Difference lists"
+        [ test "empty list is empty" <|
+            \() ->
+                equal [] <|
+                    toList empty
+        , test "singleton list contains one thing" <|
+            \() ->
+                equal [ 19 ] <|
+                    toList <|
+                        singleton 19
+        , test "from-to empty list" <|
+            \() ->
+                equal [] <|
+                    toList <|
+                        fromList []
+        , test "from-to non-empty list" <|
+            \() ->
+                equal [ 18, 21, 74, 8 ] <|
+                    toList <|
+                        fromList [ 18, 21, 74, 8 ]
+        , test "cons onto empty list" <|
+            \() ->
+                equal [ 23 ] <|
+                    toList <|
+                        cons 23 empty
+        , test "cons onto non-empty list" <|
+            \() ->
+                equal [ 40, 21, 18 ] <|
+                    toList <|
+                        cons 40 <|
+                            fromList [ 21, 18 ]
+        , test "snoc onto empty list" <|
+            \() ->
+                equal [ 23 ] <|
+                    toList <|
+                        snoc empty 23
+        , test "snoc onto non-empty list" <|
+            \() ->
+                equal [ 21, 18, 40 ] <|
+                    toList <|
+                        snoc (fromList [ 21, 18 ]) 40
+        , test "append two non-empty lists" <|
+            \() ->
+                equal [ 34, 18, 91, 70 ] <|
+                    toList <|
+                        append (fromList [ 34, 18 ]) (fromList [ 91, 70 ])
+        , test "append three singleton lists, right-associative" <|
+            \() ->
+                equal [ 18, 19, 20 ] <|
+                    toList <|
+                        append (singleton 18) <|
+                            append (singleton 19) (singleton 20)
+        , test "append three singleton lists, left-associative" <|
+            \() ->
+                equal [ 18, 19, 20 ] <|
+                    toList <|
+                        append (append (singleton 18) (singleton 19)) (singleton 20)
+        , test "append with empty list on left" <|
+            \() ->
+                equal [ 40, 21 ] <|
+                    toList <|
+                        append empty (fromList [ 40, 21 ])
+        , test "append with empty list on right" <|
+            \() ->
+                equal [ 40, 21 ] <|
+                    toList <|
+                        append (fromList [ 40, 21 ]) empty
+        , test "append two empty lists" <|
+            \() ->
+                equal [] <|
+                    toList <|
+                        append empty empty
+        , test "map over a non-empty list" <|
+            \() ->
+                equal [ "21", "88", "93" ] <|
+                    toList <|
+                        map String.fromInt <|
+                            fromList [ 21, 88, 93 ]
+        , test "fold over an empty list" <|
+            \() ->
+                equal 1 <|
+                    foldr (*) 1 empty
+        , test "fold over non-empty list" <|
+            \() ->
+                equal 120 <|
+                    foldr (*) 1 <|
+                        fromList [ 5, 4, 3, 2 ]
+        , test "concat a list of dlists" <|
+            \() ->
+                equal [ 13, 81, 99, 40 ] <|
+                    toList <|
+                        concat
+                            [ singleton 13
+                            , empty
+                            , fromList [ 81, 99 ]
+                            , empty
+                            , singleton 40
+                            , empty
+                            ]
+        , test "intersperse a list into a list of dlists" <|
+            \() ->
+                equal [ 15, 0, 81, 17, 0, 99 ] <|
+                    toList <|
+                        intersperse (singleton 0)
+                            [ singleton 15
+                            , fromList [ 81, 17 ]
+                            , singleton 99
+                            ]
         ]
